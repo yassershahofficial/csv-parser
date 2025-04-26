cd656b7a0d12d6c7c94769cefd773b4c09245385
yassershahofficial <geniususer4u@gmail.com>
2025-04-26 12:17:41 +0800

    table 2

--- public/js/index.js
+++ public/js/index.js
@@ -3,7 +3,7 @@ fetch('/csv-data')
         return res.json()
     })
     .then(data => {
-        console.log(data);
+        // console.log(data);
 
         const table1 = document.querySelector('#table1 tbody');
         table1.innerHTML = '';
@@ -18,6 +18,34 @@ fetch('/csv-data')
 
         const table2 = document.querySelector('#table2 tbody');
         table2.innerHTML = '';
+        dataMap = {};
+        data.forEach(row => {
+            dataMap[row['Index #']] = parseFloat(row['Value']);
+        });
+
+        // console.log(dataMap);
+        const table2_row = [
+            {
+                category : 'Alpha',
+                value : dataMap['A5'] + dataMap['A20']
+            },
+            {
+                category : 'Beta',
+                value : dataMap['A15'] / dataMap['A7']
+            },
+            {
+                category : 'Charlie',
+                value : dataMap['A13'] * dataMap['A12']
+            }
+        ];
+
+        table2_row.forEach(row => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = 
+                `<td>${row.category}</td>
+                <td>${row.value}</td>`;
+            table2.appendChild(tr);
+        })
     })
     .catch(err => {
         console.log(err)
